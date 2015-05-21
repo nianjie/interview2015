439d9b1c99ebaa12511c8c415bbc34986b86cb23
developer1 <dev1@nj-MacBook-Pro.local>
2015-05-21 13:08:33 +0900

    add comments.

--- src/findcommonancestor/MyFindCommonAncestor.java
+++ src/findcommonancestor/MyFindCommonAncestor.java
@@ -19,12 +19,15 @@ public class MyFindCommonAncestor implements FindCommonAncestor {
 	@Override
 	public String findCommmonAncestor(String[] commitHashes,
 			String[][] parentHashes, String commitHash1, String commitHash2) {
-
+		// use set to hold all ancestors for a specified commit.
 		Set<String> hash1Ancestors = new HashSet<String>(Arrays.asList(commitHash1));
 		Set<String> hash2Ancestors = new HashSet<String>(Arrays.asList(commitHash2));
 
 		for (int i = 0; i < commitHashes.length; i++) {
 			if (parentHashes[i] != null ) {
+				// if a commit at index i can be found,
+				// then commits held in parentHases at the same index should
+				// be the corresponding ancestors immediately or not immediately
 				if (hash1Ancestors.contains(commitHashes[i])) {
 					hash1Ancestors.addAll(Arrays.asList(parentHashes[i]));
 				}
@@ -33,10 +36,10 @@ public class MyFindCommonAncestor implements FindCommonAncestor {
 				}
 			}
 		}
-		//
+		//remove given commits selves
 		hash1Ancestors.remove(commitHash1);
 		hash2Ancestors.remove(commitHash2);
-		//
+		//by performing intersection common ancestors can be found.
 		if (hash1Ancestors.retainAll(hash2Ancestors)) {
 			return mostRecentAncestor(commitHashes, hash1Ancestors);
 		}
